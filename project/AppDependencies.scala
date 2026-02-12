8a7a413b905d3d0a8b4d1e01d6565bd1069e47de
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2026-02-12 19:00:01 +0000

    fix: Version 4.1.0 of internal-auth-client made changes that mean that a controller cannot be tested.
    The body parser can either be fixed return a fixed result - either no content to be parsed or the
    content to be hardcoded which will not allow for the testing of a variety of scenarios.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,12 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "12.29.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "4.3.0"
+
+    /* Warning: Version 4.1.0 made changes that mean that a controller cannot be tested.
+     * The body parser can either be fixed return a fixed result - either no content to be parsed or the content to
+     * be hardcoded which will not allow for the testing of a variety of scenarios.
+     */
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "4.0.0"
   )
 
   val test = Seq(
